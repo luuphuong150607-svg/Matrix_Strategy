c7aff347838b992fcac3792a31110d076b6b66cf
phuong <luuphuong150607@gmail.com>
2026-05-13 22:53:04 +0700

    Fix lỗi đường dẫn bằng File.separator

--- src/test/java/FilePathTest.java
+++ src/test/java/FilePathTest.java
@@ -15,7 +15,7 @@ public class FilePathTest {
         String systemPath = new File(directory, filename).getPath();
 
         // Cách 1: Tái cấu trúc dùng File.separator (Cách cổ điển)
-        String refactoredPath1 = directory + "/" + filename;
+        String refactoredPath1 = directory + File.separator + filename;
         assertEquals(systemPath, refactoredPath1, "Cach 1 that bai!");
 
         // Cách 2: Tái cấu trúc dùng API java.nio.file.Path
